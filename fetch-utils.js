1dc939ee38b3832c40e6e9ce5c2c49cf944be11f
JeffAllison <itsjallison@gmail.com>
2022-03-22 11:41:10 -0700

    added candies SB list to fetchUjs, added placeholder HTML for dynamic elements

---
+++ fetch-utils.js
@@ -0,0 +1,12 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYnpocGxkanJ4cWtqc2tjaXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTIwMDMsImV4cCI6MTk2MzEyODAwM30.idE1m2ehmckSIic7mOSaXFl1McMzBdIrhU_Vrsr6UyI';
+const SUPABASE_URL = 'https://lrbzhpldjrxqkjskcizc.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getCandies() {
+  const response = await client
+    .from('candies')
+    .select('*');
+
+  return response.body;
+}
